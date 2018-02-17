9afa06494d8a59430428714d4ff8916fd1fd4f06
Juliano <juliano.brasil@gmail.com>
2018-02-17 17:38:45 -0200

    exclude .ng_build folder from commits (#2)

--- .ng_build/es2015/multi-datepicker.js
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { MultiDatepicker as ɵa } from './multidatepicker/mutli-datepicker.component';
-//# sourceMappingURL=multi-datepicker.js.map
--- .ng_build/es2015/public_api.js
+++
@@ -1,2 +0,0 @@
-export { MultiDatepickerModule } from './multidatepicker/multi-datepicker.module';
-//# sourceMappingURL=public_api.js.map
